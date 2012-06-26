e4891ce9232fde4d53b26c82bed1036e47c43f41
Paramjit Raloowall <raloowallp@gmail.com>
2012-06-26 17:23:00 -0500

    claning the solution

--- Search.Tests/DoStrategyQueryTest.cs
+++ Search.Tests/DoStrategyQueryTest.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
-using Core;
-using Core.StrategyPattern;
 using Core.common;
+using Infrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
@@ -11,11 +10,11 @@ namespace Search.Tests
    [TestClass()]
     public class DoStrategyQueryTest
    {
-       private readonly IList<String> searchlist;
+       private readonly IList<String> _searchlist;
 
        public DoStrategyQueryTest()
         {
-            searchlist = new List<string> {"1", "1", "2", "3", "4"};
+            _searchlist = new List<string> {"1", "1", "2", "3", "4"};
         }
 
        [TestMethod]
@@ -24,7 +23,7 @@ namespace Search.Tests
             IList<SearchElement<string>> list = new List<SearchElement<string>>();
             list.Add(new SearchElement<string>() { Obj = "1", Oprs = OperatorType.Equals });
            
-           var search = new DoStrategyQuery<String>(list, searchlist.AsQueryable(),SearchFactory.GetInstanceOfSearchFactoru());
+           var search = new DoStrategyQuery<String>(list, _searchlist.AsQueryable(),SearchFactory.GetInstanceOfSearchFactoru());
             Assert.AreEqual(2, search.ExecuteSearch().Count());
        } 
        [TestMethod]
@@ -33,7 +32,7 @@ namespace Search.Tests
             IList<SearchElement<string>> list = new List<SearchElement<string>>();
             list.Add(new SearchElement<string>() { Obj = "1", Oprs = OperatorType.NotEquals });
 
-            var search = new DoStrategyQuery<String>(list, searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
+            var search = new DoStrategyQuery<String>(list, _searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
             Assert.AreEqual(3, search.ExecuteSearch().Count());
        }
 
@@ -43,7 +42,7 @@ namespace Search.Tests
            IList<SearchElement<string>> list = new List<SearchElement<string>>();
            list.Add(new SearchElement<string>() { Obj = "2", Oprs = OperatorType.LessThan });
 
-           var search = new DoStrategyQuery<String>(list, searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
+           var search = new DoStrategyQuery<String>(list, _searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
            Assert.AreEqual(2, search.ExecuteSearch().Count());
        }
 
@@ -53,7 +52,7 @@ namespace Search.Tests
            IList<SearchElement<string>> list = new List<SearchElement<string>>();
            list.Add(new SearchElement<string>() { Obj = "1", Oprs = OperatorType.Greaterthan });
 
-           var search = new DoStrategyQuery<String>(list, searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
+           var search = new DoStrategyQuery<String>(list, _searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
            Assert.AreEqual(3, search.ExecuteSearch().Count());
        }
 
@@ -63,7 +62,7 @@ namespace Search.Tests
            IList<SearchElement<string>> list = new List<SearchElement<string>>();
            list.Add(new SearchElement<string>() { Obj = "2", Oprs = OperatorType.LessThanEquals });
 
-           var search = new DoStrategyQuery<String>(list, searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
+           var search = new DoStrategyQuery<String>(list, _searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
            Assert.AreEqual(3, search.ExecuteSearch().Count());
        }
 
@@ -73,7 +72,7 @@ namespace Search.Tests
            IList<SearchElement<string>> list = new List<SearchElement<string>>();
            list.Add(new SearchElement<string>() { Obj = "2", Oprs = OperatorType.GreaterthanEquals });
 
-           var search = new DoStrategyQuery<String>(list, searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
+           var search = new DoStrategyQuery<String>(list, _searchlist.AsQueryable(), SearchFactory.GetInstanceOfSearchFactoru());
            Assert.AreEqual(3, search.ExecuteSearch().Count());
        }
 
